75c1c385a1a5dcd5d9e898e5cacd54a544ddf75f
Narek Tamazyan <100186064+realnarek@users.noreply.github.com>
2026-04-15 23:07:53 +0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+// PASTE YOUR CLAUDE CODE HERE
+
+export default function App() {
+  return (
+    <div style={{textAlign: "center", marginTop: "100px"}}>
+      <h1>Paste your Sushi Invitation JSX here</h1>
+    </div>
+  )
+}
